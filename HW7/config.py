c95437f2e8505eb67a5f438debd6e509f6db3e8b
ijm <ijmerai@gmail.com>
2018-09-04 14:57:41 -0500

    Commit work

---
+++ HW7/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "VTguEDeYRxKSjZ74nhfSPilwy"
+consumer_secret = "Ta68zi9fF1l4TRIE5F5E2BSZfre5LBfMD7YcCFbdCkgoFLHOrJ"
+access_token = "1032765576735797248-pOgSq1QZcgCPj6pcpTIGZaRRnwidb1"
+access_token_secret = "a8DzLn7F0uBMm3n2Hfq5tFf8J4lGaZ3fXaN099eIb5ahH"
